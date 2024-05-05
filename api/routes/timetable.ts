6b339d0be07149488540b95018ef9cb195f12ba7
Marcus <marcusnirgi@gmail.com>
2024-05-05 22:44:40 +0300

    api docs wip

--- api/routes/timetable.ts
+++ api/routes/timetable.ts
@@ -1,13 +1,24 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Timetable:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *         - finished
+ */
+
 import { Router } from "express";
 import { timetableService } from "../../serviceProvider";
 
 const TimetableRouter = Router();
 
 
-
-
 TimetableRouter.get("/", (req, res) => {
     res.send(Object.values(timetableService.timetableStores).map(ttStore => ttStore.dto));
 })
+
 
 export default TimetableRouter;
--- api/server.ts
+++ api/server.ts
@@ -7,6 +7,9 @@ import TimetableChangesRouter from "./routes/ttChanges";
 import SubjectsRouter from "./routes/subject";
 import LessonsRouter from "./routes/lesson";
 
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
 const app: Express = express();
 app.use(express.json());
 
@@ -18,5 +21,29 @@ TimetableRouter.use("/:timetableId/teachers", TimetableMiddleware, TeacherRouter
 TimetableRouter.use("/:timetableId/subjects", TimetableMiddleware, SubjectsRouter);
 TimetableRouter.use("/:timetableId/lessons", TimetableMiddleware, LessonsRouter);
 
+const specs = swaggerJSDoc({
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "TTHK Timetable API",
+            version: "0.1.0",
+            description:
+                "This is the unofficial TTHK school API"
+        },
+        servers: [
+            {
+                url: "http://localhost:" + process.env.API_PORT + "/api",
+            },
+        ],
+    },
+    apis: ["./api/routes/*.ts"]
+})
+
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
 
 export default app;
